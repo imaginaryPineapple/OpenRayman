56fda34225a1a3acdce02ac2cef3f125e619ff03
Hannes Mann <hannesmann2000@gmail.com>
2016-06-11 21:19:43 +0200

    Copy data directory into binary directory

--- src/engine.cc
+++ src/engine.cc
@@ -5,6 +5,5 @@
 
 int openrayman::engine::run(const std::string& game)
 {
-    openrayman::message_box::display("Test title", "This is a test of the message_box class.\nMultilines are supported.", false);
     return EXIT_SUCCESS;
 }
