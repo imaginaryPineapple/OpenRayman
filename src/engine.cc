443b6630f5641d660320dd3f9f0f01a1a8befb53
Hannes Mann <hannesmann2000@gmail.com>
2016-06-11 22:40:36 +0200

    Improve stub

--- src/engine.cc
+++ src/engine.cc
@@ -10,23 +10,34 @@ int openrayman::engine::run(const std::string& game)
 {
     if(!glfwInit())
     {
+        std::cout << "The GLFW library could not be initialized." << std::endl << "Are your graphics drivers up to date?" << std::endl;
         openrayman::message_box::display("Error!", "The GLFW library could not be initialized.\nAre your graphics drivers up to date?", true);
         return EXIT_FAILURE;
     }
-    GLFWwindow* window = glfwCreateWindow(1024, 768, ("OpenRayman " + openrayman::version + " (game \"" + game + "\")").c_str(), nullptr, nullptr);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE , GLFW_TRUE);
+    GLFWwindow* window = glfwCreateWindow(1024, 768, ("OpenRayman " + openrayman::version + " (game \"" + game + "\") OpenGL").c_str(), nullptr, nullptr);
     if(!window)
     {
         glfwTerminate();
+        std::cout << "The GLFW window could not be created." << std::endl << "Are your graphics drivers up to date?" << std::endl;
+        openrayman::message_box::display("Error!", "The GLFW window could not be created.\nAre your graphics drivers up to date?", true);
         return EXIT_FAILURE;
     }
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(0);
     // TODO: png loader, set icon
     while(!m_exit_requested)
     {
         glClear(GL_COLOR_BUFFER_BIT);
         glfwPollEvents();
         glfwSwapBuffers(window);
+        glFinish();
         m_exit_requested = m_exit_requested || glfwWindowShouldClose(window);
     }
+    glfwDestroyWindow(window);
     glfwTerminate();
     return EXIT_SUCCESS;
 }
