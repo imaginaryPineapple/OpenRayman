6d8893f65d0c5a08cdf4c0fadea0bec7c375bbc0
Hannes Mann <hannesmann2000@gmail.com>
2016-06-12 22:33:35 +0200

    Add input_state structure

--- src/input/input_state.h
+++ src/input/input_state.h
@@ -1,29 +1,44 @@
 #ifndef INPUT_STATE_H
 #define INPUT_STATE_H
 
+#include <cstdint>
 #include <string>
 
 namespace openrayman
 {
-    // Multi platform abstraction for filesystem access.
-    class file
+    // Buttons on a N64 controller.
+    enum input_button : std::uint16_t
     {
-public:
-        // If the specified file or directory exists.
-        static bool exists(const std::string& path);
+        dpad_left = (1 << 0),
+        dpad_right = (1 << 1),
+        dpad_up = (1 << 2),
+        dpad_down = (1 << 3),
+        start = (1 << 4),
+        a = (1 << 5),
+        b = (1 << 6),
+        l = (1 << 7),
+        r = (1 << 8),
+        z = (1 << 9),
+        cbtn_left = (1 << 10),
+        cbtn_right = (1 << 11),
+        cbtn_up = (1 << 12),
+        cbtn_down = (1 << 13)
+    };
 
-        // Replaces all instances of / and \ in the specified string with the current path separator.
-        static const std::string fix_string(const std::string& string);
+    // State of input at a point in time.
+    // Input is mapped to an N64 controller layout (the original Rayman 2 console).
+    struct input_state
+    {
+        input_button buttons;
+        int8_t stick_x, stick_y;
 
-        // Creates a new directory with the specified path.
-        static void create_directory(const std::string& path);
+        input_state(input_button buttons, int8_t stick_x, int8_t stick_y) :
+            buttons(buttons), stick_x(stick_x), stick_y(stick_y) { }
 
-        // The path separator of the current platform.
-#ifdef _WIN32
-        static const char path_separator = '\\';
-#else
-        static const char path_separator = '/';
-#endif
+        bool button(input_button button)
+        {
+            return (buttons & button) == button;
+        }
     };
 }
 
