dd76a338212f0fa749c11b45cac0ba3c9d9256bf
Hannes Mann <hannesmann2000@gmail.com>
2016-06-13 03:59:36 +0200

    Add input commands for additional actions such as fullscreen

--- src/input/input_state.h
+++ src/input/input_state.h
@@ -9,35 +9,73 @@ namespace openrayman
     // Buttons on a N64 controller.
     enum input_button : std::uint16_t
     {
-        dpad_left = (1 << 0),
-        dpad_right = (1 << 1),
-        dpad_up = (1 << 2),
-        dpad_down = (1 << 3),
-        start = (1 << 4),
-        a = (1 << 5),
-        b = (1 << 6),
-        l = (1 << 7),
-        r = (1 << 8),
-        z = (1 << 9),
-        cbtn_left = (1 << 10),
-        cbtn_right = (1 << 11),
-        cbtn_up = (1 << 12),
-        cbtn_down = (1 << 13)
+        // No button. This should not be combined with other values.
+        none = (1 << 0),
+
+        // All four dpad axises.
+        dpad_left = (1 << 1),
+        dpad_right = (1 << 2),
+        dpad_up = (1 << 3),
+        dpad_down = (1 << 4),
+
+        // The start button. Used for pausing.
+        start = (1 << 5),
+
+        // The front buttons.
+        a = (1 << 6),
+        b = (1 << 7),
+
+        // The triggers.
+        l = (1 << 8),
+        r = (1 << 9),
+        z = (1 << 10),
+
+        // All four C-buttons.
+        cbtn_left = (1 << 11),
+        cbtn_right = (1 << 12),
+        cbtn_up = (1 << 13),
+        cbtn_down = (1 << 14)
+    };
+
+    // Additional and optional commands that can be sent by an input state (for fullscreen, etc).
+    enum input_command : std::uint8_t
+    {
+        // No command. This is the default command for all game controllers and should not be combined with other values.
+        none = (1 << 0),
+
+        // Requests that the engine toggles fullscreen mode. Is not honored when running as a libretro core.
+        toggle_fullscreen = (1 << 1)
     };
 
     // State of input at a point in time.
     // Input is mapped to an N64 controller layout (the original Rayman 2 console).
     struct input_state
     {
+    	// The buttons that are held down.
         input_button buttons;
+
+        // Additional commands.
+        input_command commands;
+
+        // The analog stick axises, from -128 to 127.
         int8_t stick_x, stick_y;
 
         input_state(input_button buttons, int8_t stick_x, int8_t stick_y) :
-            buttons(buttons), stick_x(stick_x), stick_y(stick_y) { }
+            buttons(buttons), commands(input_command::none), stick_x(stick_x), stick_y(stick_y) { }
 
-        bool button(input_button button)
+        input_state(input_button buttons, input_command commands, int8_t stick_x, int8_t stick_y) :
+            buttons(buttons), commands(commands), stick_x(stick_x), stick_y(stick_y) { }
+
+        // Helper function to determine if the specified button is held down.
+        inline bool button(input_button button)
         {
             return (buttons & button) == button;
+        }
+
+        // Helper function to determine if a command was specified.
+        inline bool command(input_command command)
+        {
+            return (commands & command) == command;
         }
     };
 }
